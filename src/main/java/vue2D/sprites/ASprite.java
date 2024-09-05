dc38f36ecb040390feff4b6dd13c57663ac1eb2c
Oceane Jean-Francois <ojfrancois@IUT.BX>
2024-09-05 22:21:31 +0200

    implémentation de la classe ASprite (exercice 14)

---
+++ src/main/java/vue2D/sprites/ASprite.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vue2D.sprites;
+
+import java.util.Collection;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import labyrinthe.ISalle;
+import personnages.IPersonnage;
+import static vue2D.AVue.UNITE;
+
+/**
+ *
+ * @author ojfrancois
+ */
+public abstract class ASprite implements ISprite {
+  
+    private IPersonnage perso;
+    private int xpix ;
+    private int ypix ;
+    private Image image;
+
+    public ASprite(IPersonnage perso) {
+        this.perso = perso;
+    }
+    
+      public IPersonnage getPerso() {
+        return perso;
+    }
+
+    public void setPerso(IPersonnage perso) {
+        this.perso = perso;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+
+    @Override
+    public void dessiner(GraphicsContext g) {
+      
+        g.drawImage(image,xpix, ypix , UNITE,UNITE);
+          
+    }
+
+    @Override
+    public void setCoordonnees(int xpix, int ypix) {
+        this.xpix = xpix;
+         this.ypix = ypix;
+    }
+
+    
+    
+}
