89c1c9d747bc976bb73db538158ae02d4ed13b9d
Oceane Jean-Francois <ojfrancois@IUT.BX>
2024-09-05 20:47:46 +0200

    implémentation de la classe APersonnage (exercice12)

---
+++ src/main/java/personnages/APersonnage.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personnages;
+
+import java.util.Collection;
+import labyrinthe.ISalle;
+
+/**
+ *
+ * @author ojfrancois
+ */
+public abstract class APersonnage implements IPersonnage{
+
+  ISalle position;
+
+    public APersonnage(ISalle position) {
+        this.position = position;
+    }
+    
+    @Override
+      public ISalle getPosition() {
+        return position;
+    }
+      
+    @Override
+    public void setPosition(ISalle position) {
+        this.position = position;
+    }
+
+    @Override
+    public ISalle faitSonChoix(Collection<ISalle> sallesAccessibles) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
